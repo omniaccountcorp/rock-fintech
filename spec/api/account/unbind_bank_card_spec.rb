d1b7b3316775ea2c61a8424b1c260f9bbb45ec7f
chenpengyu <chpy15@gmail.com>
2018-05-03 20:34:54 +0800

    增加解绑卡 spec

--- spec/api/account/unbind_bank_card_spec.rb
+++ spec/api/account/unbind_bank_card_spec.rb
@@ -5,10 +5,13 @@ RSpec.describe '解绑银行卡' do
   let(:card_no) { '12312312' }
   let(:bank_card_no) { '6212262201023557228' }
   let(:customer_no) { '123123123' }
+  let(:flow_id) { RockFintech::Utils.gen_flow_id }
+  let(:card_type) { '0' }
 
   it '返回错误信息' do
-    res = client.account_balance(card_no, customer_no)
+    res = client.unbind_bank_card(card_no, bank_card_no, customer_no, flow_id, card_type)
 
-    expect(res[:code]).to eq('RD000006')
+    puts res
+    expect(res[:code]).to eq('RD000094')
   end
 end
