d99f31b25a555059731ee7d4764ff10810ebd57d
chenpengyu <chpy15@gmail.com>
2018-05-03 16:48:51 +0800

    增加 error code

--- lib/rock_fintech/http/error_code/account.rb
+++ lib/rock_fintech/http/error_code/account.rb
@@ -8,6 +8,19 @@ module RockFintech
         @_create_account ||= %w[RD000601 RD000602 RD000603 RD000604 RD000605 RD777777 RD10001]
       end
 
+      def self.set_password_p
+        @_set_password ||= %w[RD1031303 RD1031304 RD10001 RD001034 RD000602 RD001024 RD9997
+                              RD1031301 RD000603 RD000604 RD000605]
+      end
+
+      def self.bind_bank_card_p
+        @_bind_bank_card ||= []
+      end
+
+      def self.account_balance
+        @_account_balance ||= []
+      end
+
     end # module ErrorCode
   end
 end
---
+++ lib/rock_fintech/http/error_code/money.rb
@@ -0,0 +1,21 @@
+# coding: utf-8
+
+module RockFintech
+  module Http
+    module ErrorCode
+
+      def self.bank_recharge
+        @_bank_recharge ||= []
+      end
+
+      def self.recharge_p
+        @_recharge_p ||= []
+      end
+
+      def self.withdraw_p
+        @_withdraw_p ||= []
+      end
+
+    end # module ErrorCode
+  end
+end
