1d9fada86ff9f5608ca571981780abefdd8afbcb
peterswj <peterswj@163.com>
2018-05-05 00:27:59 +0800

    修复红包撤销spec

--- spec/api/account/account_credits_spec.rb
+++ spec/api/account/account_credits_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe '查询投资人电子账户名下的投资明细' do
   let(:buy_date) { "" }
   it '成功查询' do
     result = client.account_credits( card_no, asset_no, state, page_flag, buy_date, "", "" )
-    ap result
+    # ap result
     aggregate_failures do
       expect(result[:code]).to_not eq nil
       expect(result[:data][:timestamp]).to_not eq nil
