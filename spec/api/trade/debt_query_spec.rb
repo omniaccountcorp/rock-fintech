57746d59f732b15ee01e89b25e8d8855def8dd1b
peterswj <peterswj@163.com>
2018-05-04 20:22:02 +0800

    spec 标的投标详情

--- spec/api/trade/debt_query_spec.rb
+++ spec/api/trade/debt_query_spec.rb
@@ -2,8 +2,14 @@
 require 'spec_helper'
 
 RSpec.describe '标的投标详情' do
-  skip '成功查询' do
-
+  let(:card_no) { "86666882002000446801" }
+  let(:asset_no) { 20 }
+  it '成功查询' do
+    result = client.debt_query(card_no, asset_no)
+    ap result
+    aggregate_failures do
+      expect(result[:code]).to_not eq nil
+      expect(result[:data][:service]).to eq "debt_query"
+    end
   end
-
 end
