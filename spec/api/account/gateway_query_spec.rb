8b664d7dcd1474cc44bbe63d6f44abd35ca1cc00
peterswj <peterswj@163.com>
2018-05-04 20:09:46 +0800

    spec 网关自定义下单查询

--- spec/api/account/gateway_query_spec.rb
+++ spec/api/account/gateway_query_spec.rb
@@ -2,8 +2,13 @@
 require 'spec_helper'
 
 RSpec.describe '网关自定义下单查询（查询内容：投标，签约，债转，受托支付申请)' do
-  skip '成功查询' do
-
+  it '成功查询' do
+    result = client.gateway_query(30)
+    # ap result
+    aggregate_failures do
+      expect(result[:code]).to_not eq nil
+      expect(result[:data][:service]).to eq "gateway_query"
+    end
   end
 
 end
