64aebea7a39696a5889aa17f136b9f03be96e70d
peterswj <peterswj@163.com>
2018-05-04 12:03:17 +0800

    查询投资人自动投标签约状态

---
+++ lib/rock_fintech/http/error_code/trade.rb
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+module RockFintech
+  module Http
+    module ErrorCode
+
+      def self.sign_bid_query
+        @_sign_bid_query||= %w[]
+      end
+    end # module ErrorCode
+  end
+end
---
+++ spec/api/trade/sign_bid_query_spec.rb
@@ -0,0 +1,9 @@
+# coding: utf-8
+require 'spec_helper'
+
+RSpec.describe '查询投资人自动投标签约状态' do
+  skip '成功查询' do
+
+  end
+
+end
