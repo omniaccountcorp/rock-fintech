f225611f66124fa42c1c38a28e1407dfebd845bb
peterswj <peterswj@163.com>
2018-05-04 20:15:58 +0800

    spec 查询投资人自动投标签约状态

--- spec/api/trade/sign_bid_query_spec.rb
+++ spec/api/trade/sign_bid_query_spec.rb
@@ -2,8 +2,14 @@
 require 'spec_helper'
 
 RSpec.describe '查询投资人自动投标签约状态' do
-  skip '成功查询' do
-
+  let(:card_no) { "86666882002000446801" }
+  it '成功查询' do
+    result = client.sign_bid_query(card_no)
+    # ap result
+    aggregate_failures do
+      expect(result[:code]).to_not eq nil
+      expect(result[:data][:service]).to eq "sign_bid_query"
+    end
   end
 
 end
