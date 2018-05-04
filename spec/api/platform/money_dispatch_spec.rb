2b28a231c4ebde98bc0f67ae95636d6daac060a6
peterswj <peterswj@163.com>
2018-05-04 15:04:14 +0800

    红包发放与撤销

---
+++ spec/api/platform/money_dispatch_spec.rb
@@ -0,0 +1,9 @@
+# coding: utf-8
+require 'spec_helper'
+
+RSpec.describe 'P2P产品红包发放' do
+  skip '发放成功' do
+
+  end
+
+end
---
+++ spec/api/platform/money_revoke_spec.rb
@@ -0,0 +1,9 @@
+# coding: utf-8
+require 'spec_helper'
+
+RSpec.describe 'P2P产品红包发放撤销' do
+  skip '撤销成功' do
+
+  end
+
+end
