659b0c898d7f419cf5e9cfbcb0fc3b5febe2664a
tony <tony@omniaccount.com>
2018-05-04 13:26:41 +0800

    充值测试

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,7 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |file| require file }
 
 Faker::Config.locale = 'zh-CN'
 
-RockFintech.logger = Logger.new('tmp/test.log')
+# RockFintech.logger = Logger.new('tmp/test.log')
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
