d1896fcb9e8c766bd3c282c1e917cabad1b072cc
peterswj <peterswj@163.com>
2018-05-29 15:07:15 +0800

    fix bugs

--- spec/api/platform/money_revoke_spec.rb
+++ spec/api/platform/money_revoke_spec.rb
@@ -2,15 +2,15 @@
 require 'spec_helper'
 
 RSpec.describe 'P2P产品红包发放撤销' do
-  let(:card_no) { "86666882002000446801" }
+  let(:card_no) { "6666882121100034167" }
   let(:card_no_in) { "86666882002000446801" }
 
   it '撤销失败' do
-    result = client.money_revoke('20180504', "6691fd7db6d0091b4cf8d6d1f05c644b", card_no, card_no_in, 3, 2000, 0, '')
+    result = client.money_revoke('20180909', "9830eab0ea908d2d3d80a2f9ec4d9", card_no, card_no_in, 3, 1000, 0, '')
     # ap result
     aggregate_failures do
       expect(result[:code]).to_not eq nil
-      expect(result[:data][:msg]).to eq "红包发放记录不存在"
+      expect(result[:data][:msg]).to eq "该笔红包撤销流水已处理"
       expect(result[:data][:service]).to eq "money_revoke"
     end
   end
