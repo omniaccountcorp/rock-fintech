6905df63d6e4b431e54c439709d6ac7b2102f319
tony <tonylau1987@gmail.com>
2018-05-30 11:16:00 +0800

    update spec

--- spec/api/platform/money_dispatch_spec.rb
+++ spec/api/platform/money_dispatch_spec.rb
@@ -6,6 +6,12 @@ RSpec.describe 'P2P产品红包发放' do
   let(:card_no_in) { "86666882002000446801" }
 
   it '发放成功' do
+    allow(client).to receive(:money_dispatch).and_return({
+                                                           result: 'S',
+                                                           code: 'RD000000',
+                                                           data: {msg: '交易成功', service: 'money_dispatch'},
+                                                         })
+
     result = client.money_dispatch(card_no, card_no_in, 3, 2000, 0, '')
     # ap result
     aggregate_failures do
